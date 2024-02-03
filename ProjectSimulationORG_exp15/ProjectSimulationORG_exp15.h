8f76b85d25f08a719a941bedbc9e18358ddc8a8f
Haruki Isono <i.haruki2203@gmail.com>
2024-02-03 12:38:10 +0900

    プロジェクト ファイルを追加します。

---
+++ ProjectSimulationORG_exp15/ProjectSimulationORG_exp15.h
@@ -0,0 +1,32 @@
+﻿
+// ProjectSimulationORG_exp15.h : PROJECT_NAME アプリケーションのメイン ヘッダー ファイルです
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "PCH に対してこのファイルをインクルードする前に 'pch.h' をインクルードしてください"
+#endif
+
+#include "resource.h"		// メイン シンボル
+
+
+// CProjectSimulationORGexp15App:
+// このクラスの実装については、ProjectSimulationORG_exp15.cpp を参照してください
+//
+
+class CProjectSimulationORGexp15App : public CWinApp
+{
+public:
+	CProjectSimulationORGexp15App();
+
+// オーバーライド
+public:
+	virtual BOOL InitInstance();
+
+// 実装
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CProjectSimulationORGexp15App theApp;
---
+++ ProjectSimulationORG_exp15/ProjectSimulationORG_exp15Dlg.h
@@ -0,0 +1,34 @@
+﻿
+// ProjectSimulationORG_exp15Dlg.h : ヘッダー ファイル
+//
+
+#pragma once
+
+
+// CProjectSimulationORGexp15Dlg ダイアログ
+class CProjectSimulationORGexp15Dlg : public CDialogEx
+{
+// コンストラクション
+public:
+	CProjectSimulationORGexp15Dlg(CWnd* pParent = nullptr);	// 標準コンストラクター
+
+// ダイアログ データ
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_PROJECTSIMULATIONORG_EXP15_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV サポート
+
+
+// 実装
+protected:
+	HICON m_hIcon;
+
+	// 生成された、メッセージ割り当て関数
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+};
